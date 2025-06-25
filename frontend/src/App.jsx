225d8aaad1517a1321a96e1f0d0ae2c0cca2e332
jahangir Hassan <835-2019@hamdard.edu>
2025-06-25 16:54:04 +0500

    Add license information for framer-motion and react-icons; import HomePage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from './context/AuthContext';
 import MyListings from './components/MyListings';
 import AboutPage from './components/AboutPage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
